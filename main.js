3301edc6f6ea5b86b679c2d8b50fedcbbb88dceb
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-09-05 11:10:51 +0800

    changes

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerButton = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  hamburgerButton.addEventListener("click", function () {
+      hamburgerButton.classList.toggle("active");
+      mobileMenu.classList.toggle("mob-show");
+  });
+});
+
+
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
     spaceBetween: 16,
