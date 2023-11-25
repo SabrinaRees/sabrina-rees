9b4ad654acac3bf882dd235951a30d23c43b62ef
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-11-25 15:48:25 +0800

    small

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburgerButton = document.getElementById("hamburger-button");
-  const mobileMenu = document.getElementById("mobile-menu");
-
-  hamburgerButton.addEventListener("click", function () {
-      hamburgerButton.classList.toggle("active");
-      mobileMenu.classList.toggle("mob-show");
+$(document).ready(function() {
+  $('.hamburger-wrapper').click(function() {
+      $('.mobile-menu').fadeToggle(300);
+      $('.hamburger-menu').toggleClass('active');
   });
 });
 
 
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    spaceBetween: 16,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
