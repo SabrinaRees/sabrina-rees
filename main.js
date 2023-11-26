e5aaad286ba3d51ebd08069d9e694c0e11f3f40d
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-11-26 16:05:39 +0800

    index small done

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('.hamburger-wrapper').click(function() {
       $('.mobile-menu').fadeToggle(300);
-      $('.hamburger-wrapper .hamburger-menu').toggleClass('active');
+      $('.hamburger-menu').toggleClass('active');
   });
 });
